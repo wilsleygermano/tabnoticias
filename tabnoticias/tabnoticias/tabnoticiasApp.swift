c24ce98dc35ba6b3c253a1f7f444def5badc3ebe
Wilsley Germano <wilsleygermano@gmail.com>
2023-09-18 21:17:36 -0300

    initial commit

---
+++ tabnoticias/tabnoticias/tabnoticiasApp.swift
@@ -0,0 +1,17 @@
+//
+//  tabnoticiasApp.swift
+//  tabnoticias
+//
+//  Created by Wilsley Germano on 18/09/23.
+//
+
+import SwiftUI
+
+@main
+struct tabnoticiasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
